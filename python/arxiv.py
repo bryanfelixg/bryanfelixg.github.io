acfe1023e237965a9334bc04ebaa6ee05c02838f
Bryan Felix <felix077@umn.edu>
2024-11-15 16:03:38 -0600

    test

--- python/arxiv.py
+++ python/arxiv.py
@@ -13,7 +13,7 @@ def format_latex(text):
     return text
 
 # Query term
-query = "mathematical biology"
+query = "astronomy"
 
 # Directory and filename to save the output
 output_dir = "../assets/"  # Specify your desired directory
